04e277dbd6eb406378a1308ba923251e798814bd
raquel-gonzo <gonzalez.rachel.c@gmail.com>
2020-09-21 22:53:18 -0500

    rotate solution.

--- to-do2.js
+++ to-do2.js
@@ -7,7 +7,7 @@
 // such as splice().
 
 function reverse(arr) {
-  // loop thru half the array to swap the values at midpoint. 
+  // loop thru half the array to swap the values at midpoint.
   for (let i = 0; i < arr.length / 2; i++) {
     // make a temp var to store the vals from original indices.
     let temp = arr[i];
@@ -23,10 +23,10 @@ function reverse(arr) {
 // ******************************************************************
 
 // Rotate
-// Implement rotateArr(arr, shiftBy) that accepts array and offset. 
-// Shift arr’s values to the right by that amount. ‘Wrap-around’ any 
-// values that shift off array’s end to the other side, so that no data is lost. 
-// Operate in-place: given ([1,2,3],1), change the array to [3,1,2]. 
+// Implement rotateArr(arr, shiftBy) that accepts array and offset.
+// Shift arr’s values to the right by that amount. ‘Wrap-around’ any
+// values that shift off array’s end to the other side, so that no data is lost.
+// Operate in-place: given ([1,2,3],1), change the array to [3,1,2].
 // Don’t use built-in functions.
 
 // Second: allow negative shiftBy (shift L, not R).
@@ -34,3 +34,39 @@ function reverse(arr) {
 // Third: minimize memory usage. With no new array, handle arrays/shiftBys in the millions.
 
 // Fourth: minimize the touches of each element.
+
+function rotate(arr, shiftBy) {
+    let t = Math.abs(shiftBy);
+    
+    while (t > 0) {
+        if (shiftBy > 0) {
+            // if n is positive
+            let temp = arr[arr.length - 1]; // create a temp variable to store the last item in the given array.
+            // loop thru array with FOR loop starting at the END of the array.
+            for (let i = arr.length-1; i > 0; i--) { // i-- to decrease since we're starting at the end. 
+                // shift the indices over by one by assigning them to the temp.
+                arr[i] = arr[i - 1];
+                arr[i - 1] = temp;
+            }
+        } else {
+            let temp = arr[0];
+            for (let i = 0; i < arr.length - 1; i ++) {  // if n is negative, loop thru the array and push the elements to the left 
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+            }
+        }
+        t--;
+    }
+    
+    return arr;
+}
+console.log("this should be [3, 1, 2]");
+console.log(rotate([1,2,3], 1));
+console.log("this should be [2, 3, 1]");
+console.log(rotate([1,2,3], 2));
+
+console.log("this should be [2, 3, 1]");
+console.log(rotate([1,2,3], -1));
+console.log("this should be [3, 1, 2]");
+console.log(rotate([1,2,3], -2));
+
